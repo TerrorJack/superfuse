d38b596961d0bdb20e7bc47a8de0fc3b1f6ccc7c
Parker Liu <liuchangsheng30@gmail.com>
2016-10-19 08:47:08 +0800

    Change WType to UType and add some unary/trinary operators

--- src/Superfuse/Frontend.hs
+++ src/Superfuse/Frontend.hs
@@ -9,30 +9,41 @@ import GHC.TypeLits
 data IType = I8 | I16 | I32 | I64
 
 -- | Unsigned integral types
-data WType = W16 | W32 | W64
+data UType = U8 | U16 | U32 | U64
 
 -- | Floating-point types
 data FType = F32 | F64
 
 -- | Scalar types
-data SType = SIType IType | SWType WType | SFType FType
+data SType = SIType IType | SUType UType | SFType FType
 
 -- | Scalar types' corresponding Haskell types
 type family HsSType t = h | h -> t where
-    HsSType ('SIType 'I8) = Int8
+    HsSType ('SIType 'I8)  = Int8
     HsSType ('SIType 'I16) = Int16
     HsSType ('SIType 'I32) = Int32
     HsSType ('SIType 'I64) = Int64
-    HsSType ('SWType 'W16) = Word16
-    HsSType ('SWType 'W32) = Word32
-    HsSType ('SWType 'W64) = Word64
+    HsSType ('SUType 'U8)  = Word8
+    HsSType ('SUType 'U16) = Word16
+    HsSType ('SUType 'U32) = Word32
+    HsSType ('SUType 'U64) = Word64
     HsSType ('SFType 'F32) = Float
     HsSType ('SFType 'F64) = Double
+
+-- | Scalar unary operators.
+data SUnaryOp :: SType -> Type where
+    Neg :: SUnaryOp t
+    Abs :: SUnaryOp t
 
 -- | Scalar binary operators. The Bool indicates whether it satisfies monoid laws.
 data SBinOp :: SType -> Bool -> Type where
     Add, Mul :: SBinOp t 'True
     Sub, Div :: SBinOp t 'False
+    Max, Min :: SBinOp t 'True
+
+-- | Scalar trinary operators.
+data STriOp :: SType -> Type where
+    Fma :: STriOp t    -- fused multiply and add operation: a * b + c
 
 -- | Vector expressions
 data VExpr :: SType -> Nat -> Type where
